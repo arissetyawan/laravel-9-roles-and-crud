22ec233f2c1374f4fea80b10a049c968977c4bd2
halimhmairi <halim.hmairi@nostatikmedia.com>
2022-06-06 16:05:19 +0100

    clean list role

--- resources/views/dashboard/role/index.blade.php
+++ resources/views/dashboard/role/index.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
 
+
 <div class="container">
-    <a href="{{ Route('role/create') }}" class="btn btn-primary">Add new role</a>
-  <table class="table">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Role') }} <a href="{{ Route('role/create') }}" class="btn btn-primary" style="float: right;"><i class="fa fa-plus"></i></a></div>
+
+                <div class="card-body">
+                <table class="table">
     <thead>
       <tr>
         <th scope="col">#</th>
@@ -19,12 +25,19 @@
         <th scope="row">{{ $role->id }}</th>
         <td>{{ $role->name }}</td>
         <td>{{ $role->description }}</td>
-        <td><a href="{{ Route('role/edit',$role->id) }}" class="btn btn-primary">Edit</a><a href="{{ Route('role/destroy',$role->id) }}" class="btn btn-danger">delete</a></td>
+        <td>
+         <a href="{{ Route('role/edit',$role->id) }}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+         <a href="{{ Route('role/destroy',$role->id) }}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+      </td>
       </tr>
   @endforeach
     </tbody>
   </table>
-  
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+ 
 
 @endsection
