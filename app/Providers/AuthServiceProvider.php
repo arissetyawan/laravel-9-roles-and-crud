0a9ac5b90c45181ac66b1a18028ba2714d703235
Halim Hmairi <hmairihalim@gmail.com>
2022-06-08 20:52:55 +0100

    define gate role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate; 
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('is_admin',function ($user)
+        {
+            return $user->role == 'admin';
+        });
+
+        Gate::define('is_user',function ($user)
+        {
+            return $user->role == 'user';
+        });
     }
 }
