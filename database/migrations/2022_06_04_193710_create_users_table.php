c4f2d7496fb5c004fa81f18bfc8ce2669b2c8b83
Halim Hmairi <hmairihalim@gmail.com>
2022-06-08 20:52:25 +0100

    Add foreign key user -> role

--- database/migrations/2022_06_04_193710_create_users_table.php
+++ database/migrations/2022_06_04_193710_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('role_id')->nullable();
+            $table->bigInteger('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
