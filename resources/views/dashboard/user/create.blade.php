99e1a2b4a8f0cf39adc45df36c32112b9bc4706f
Halim Hmairi <hmairihalim@gmail.com>
2022-06-09 22:04:07 +0100

    remove confirm password

--- resources/views/dashboard/user/create.blade.php
+++ resources/views/dashboard/user/create.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-header">{{ __('Create user') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user/create') }}">
+                    <form method="POST" action="{{ route('user/store') }}">
                         @csrf
 
                         <div class="row mb-3">
@@ -69,15 +69,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+                        </div> 
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
